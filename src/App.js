dffb17a196a09a145e2d4542ff0db98a26556353
zoubin <403060825@qq.com>
2019-09-21 12:18:20 +0800

    添加文件

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
-import routesConfig from './router/routeConfig'
+// import routesConfig from './router/routeConfig'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          {renderRoutes(routesConfig)}
+          <Switch>
+            <Route>
+              
+            </Route>
+          </Switch>
+          {/* {renderRoutes(routesConfig)} */}
         </BrowserRouter>
       </div>
     );
